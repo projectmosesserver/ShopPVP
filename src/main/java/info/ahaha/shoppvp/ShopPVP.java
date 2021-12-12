a7700fdb6216d1d467ca9213054af6551a51b0c9
kaogon128 <bump_love128@yahoo.co.jp>
2021-12-12 18:20:32 +0900

    Initial commit

---
+++ src/main/java/info/ahaha/shoppvp/ShopPVP.java
@@ -0,0 +1,17 @@
+package info.ahaha.shoppvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ShopPVP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
